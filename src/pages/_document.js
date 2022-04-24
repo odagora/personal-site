d9befc52765ea2b8519f0011c1a601978963003e
Daniel <oscar.d.gonzalez@gmail.com>
2022-04-24 14:26:07 -0500

    [UPDATE] Google Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,6 +32,16 @@ export default class MyDocument extends Document {
         return (
             <Html {...this.helmetHtmlAttrComponents}>
                 <Head>{this.helmetHeadComponents}</Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-KCKQ7CJKFG"></script>
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-KCKQ7CJKFG');
+                    `
+                }}/>
                 <body {...this.helmetBodyAttrComponents}>
                     <Main />
                     <script src="/js/plugins.js" />
